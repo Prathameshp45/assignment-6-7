b2aed714b5c2ca2dbf97ef348de436141a9077f2
Prathameshp45 <prathameshp9456@gmail.com>
2024-08-05 13:04:52 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+import TodoList from './component/TodoList';
+import LoginForm from './component/LoginForm';
+import RegistrationForm from './component/RegistrationForm';
+import SurveyForm from './component/SurveyForm';
+import SearchFilter from './component/SearchFilter';
+import SettingsForm from './component/SettingsForm';
+import ShoppingCart from './component/ShoppingCart';
+import ImageGallery from './component/ImageGallery';
+import ContactList from './component/ContactList';
+
+
 function App() {
+  const imageUrls = [
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgnXqvD-_BPJv9HHDcyeJ3daV-DUWFB3sEug&s',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgnXqvD-_BPJv9HHDcyeJ3daV-DUWFB3sEug&s',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA1BbUFcPJ3sC1KVud8HJEHAtcK3K4adDBhg&s',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA1BbUFcPJ3sC1KVud8HJEHAtcK3K4adDBhg&s',
+'https://thumbs.dreamstime.com/b/lamborghini-veneno-supercar-mystical-forest-filled-ancient-trees-lamborghini-veneno-supercar-mystical-forest-292518568.jpg',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-7O83ZdTjYNjB_VVGpCNZm-vUhWET7DL0tKy5C0BHP4crb7wFbJeTz_F88IxF2lwlsUQ&usqp=CAU'
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <TodoList/>
+   <br></br>
+   <hr></hr>
+  < LoginForm/>
+  <br></br>
+   <hr></hr>
+   <RegistrationForm/>
+   <br></br>
+   <hr></hr>
+   <SurveyForm/>
+   <br></br>
+   <hr></hr>
+   <SearchFilter/>
+   <br></br>
+   <hr></hr>
+   <SettingsForm/>
+   <br></br>
+   <hr></hr>
+   <ShoppingCart/>
+   <br></br>
+   <hr></hr>
+   <ImageGallery imageUrls={imageUrls} />  
+   <br></br>
+   <hr></hr> 
+    <ContactList />
+    
+    </>
+
   );
 }
 
